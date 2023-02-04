1305c3d1deb23b5b159e21905efab08b55e18f37
J-724 <jjpernian@gmail.com>
2023-02-03 21:54:36 -0400

    practice done

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,36 @@ const app = express()
 
 const PORT = 5000;
 
-console.log("Hello typescript");
+app.use('/home', (req, res) =>{
+  res.send("Hello world")
+})
 
-app.listen(PORT)
+app.use('/about', (req, res) =>{
+  res.send("Hello about")
+})
+
+app.use('/contact-me', (req, res) =>{
+  res.send("Contact-me")
+})
+
+app.use('/message', (req, res) =>{
+  const message = {
+    user: 'Ryuk',
+    age: 9,
+    breed: 'mix',
+    friends: [
+      'Ora',
+      'Blonder',
+      'Perrita del metro',
+      'Perrita de toyo tachira',
+      'Funcionario publico'
+    ]
+  }
+  res.send(message)
+})
+
+app.listen(PORT, () =>{
+  console.log(`Hello typescript, listening to port: ${PORT}`);
+})
 
 
